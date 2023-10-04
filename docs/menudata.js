26aec60b6380ee0d88b2149c60532cda6bee16bc
Mehmet Cagri Aksoy <mcagriaksoy@yandex.com>
2023-10-04 20:15:13 +0200

    Doxygen add

--- docs/menudata.js
+++ docs/menudata.js
@@ -23,4 +23,7 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"}]}
+{text:"Main Page",url:"index.html"},
+{text:"Related Pages",url:"pages.html"},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"}]}]}
